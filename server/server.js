73733da705ef2007769362dc1592c4b2d4d2aec1
Akin909 <akin.sowemimo@gmail.com>
2017-07-17 14:37:29 +0100

    install initial dependencies create route files

--- server/server.js
+++ server/server.js
@@ -6,5 +6,5 @@ const app = express();
 const port = process.env.PORT || 4001;
 
 app.listen(port, () => {
-  console.log('The things, the things are happening on ${port}');
+  console.log(`The things, the things are happening on ${port}!!`);
 });
