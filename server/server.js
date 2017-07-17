259bf26005f7a0cf21f2e64a94943d5df9406bb3
Akin909 <akin.sowemimo@gmail.com>
2017-07-17 14:30:03 +0100

    initial commit

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const port = process.env.PORT || 4001;
+
+app.listen(port, () => {
+  console.log('The things, the things are happening on ${port}');
+});
