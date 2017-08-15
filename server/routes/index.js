653fd5001b7d6c5dd78d0cde84c5f70277ecf305
Akin <akin.sowemimo@gmail.com>
2017-08-15 10:20:01 +0100

    add routes to main workshop

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,30 +4,66 @@ const { Router } = express; //Here we destructure (ES6) the Router value off of 
 const router = Router();
 const queries = require('./queries');
 
-/* TODO This is an example of a route made by using the queries defined in queries.js */
+// =========================================================
+// Routes
+// =========================================================
 router.get('/facsters', (req, res, next) =>
   queries
     .getAll()
     .then(users => res.status(200).json(users))
     .catch(err => next(err))
 );
-// =========================================================
-// Create Routes Here
-// =========================================================
+
+router.get('/facsters', (req, res, next) =>
+  queries
+    .getAll()
+    .then(users => res.status(200).json(users))
+    .catch(err => next(err))
+);
 /*
  * '/facsters/:name'
  */
+router.get('/facsters/:name', ({ params: { name } }, res, next) => {
+  queries
+    .getSingleFacster(name)
+    .then(person => res.status(200).json(person))
+    .catch(err => next(err));
+});
 
 /*
  * '/facster/new'
  */
+router.post('/facster/new', ({ body }, res, next) => {
+  queries
+    .addFacster(body)
+    .then(userID => queries.getFacsterById(userID))
+    .then(user => res.status(201).json(user))
+    .catch(err => next(err));
+});
 
 /*
  * '/facsters/:name/hobby'
  */
-
+router.get('/facsters/:name/hobby', ({ params: { name }, body }, res, next) => {
+  queries
+    .getFacsterHobby(name)
+    .then(facsterAndHobby => res.status(200).json(facsterAndHobby))
+    .catch(err => next(err));
+});
 /*
  * '/facsters/:name/superpower'
  */
+router.get(
+  '/facsters/:name/superpower',
+  ({ params: { name }, body }, res, next) => {
+    queries
+      .getFacsterSuperpower(name)
+      .then(facsterAndPower => res.status(200).json(facsterAndPower))
+      .catch(err => next(err));
+  }
+);
+
+
+
 
 module.exports = router;
