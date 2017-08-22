006c3ceaa1d2d914de37af1b66979e8fb16bf1da
Matt Davis <matthewpeterdavis@gmail.com>
2017-08-22 10:06:46 +0300

    get rid of middleware in non solution file

--- solution-files/server.js
+++ solution-files/server.js
@@ -2,11 +2,16 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes');
+// require the body parser middleware
+const bodyParser = require('body-parser')
 
 const app = express();
 
 //TODO We need middleware to convert request and response payloads to an from JSON
+app.use(bodyParser.json())
 
 //TODO We need to setup a route/collection of routes
+app.use('/v1/api', routes)
+// Adding the first argument here means that all the paths in the routes file will now come after '/v1/api/'
 
 module.exports = app;
